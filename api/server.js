299b3bdb976d31292c1136e382e866ec2f07c550
Jojo Zhang <jojozhang0327@gmail.com>
2020-03-24 14:00:41 -0400

    follow along the project

--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const restricted = require("../auth/restricted-middleware.js");
 
 const server = express();
 
@@ -11,11 +12,25 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", restricted, checkRole("user"), usersRouter);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send("It's alive!");
 });
 
 module.exports = server;
+
+function checkRole(role) {
+  return (req, res, next) => {
+    if (
+      req.decodedToken &&
+      req.decodedToken.role &&
+      req.decodedToken.role.toLowerCase() === role
+    ) {
+      next();
+    } else {
+      res.status(403).json({ you: "Shall not pass!" });
+    }
+  };
+}
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,12 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken"); //<<<install this npm package
 
-const Users = require('../users/users-model.js');
+const Users = require("../users/users-model.js");
+const { jwtSecret } = require("../config/secrets.js");
 
 // for endpoints beginning with /api/auth
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
@@ -18,23 +20,39 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user); //get a token
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token, //send the token
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.role || "user",
+  };
+
+  const options = {
+    expiresIn: "1h",
+  };
+
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
